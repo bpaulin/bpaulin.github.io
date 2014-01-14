cbd4d48e2b448e0518a2acda80fef43220b1cc17
Bruno Paulin <brunopaulin@bpaulin.net>
2014-01-14 15:42:19 +0100

    drafts

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function(grunt) {
                 options: {
                     src: 'jekyll',
                     watch: true,
-                    serve: true,
+                    serve: true
                 }
             },
         },
