d47a47e83cbb68f8a42a5c84aed79c984ff81f00
Bruno Paulin <brunopaulin@bpaulin.net>
2016-05-12 13:30:40 +0000

    cleaning

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
                 reset: true,
                 reportpath: false,
                 failHard: true,
-                relaxerror: ["Bad value X-UA-Compatible for attribute http-equiv on element meta."]
+                generateReport: false
             },
             src: ['_site/**/*.html']
         },
@@ -38,28 +38,12 @@ module.exports = function(grunt) {
                 cwd: '_site/'
             }
         },
-
-        watch: {
-        },
-
-        concurrent: {
-            dev: ['watch', 'jekyll:serve'],
-            options: {
-                logConcurrentOutput: true
-            }
-        }
     });
 
     grunt.loadNpmTasks("grunt-jekyll");
-    grunt.loadNpmTasks("grunt-html-validation");
+    grunt.loadNpmTasks('grunt-w3c-html-validation');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-xsltproc');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-concurrent');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-shell');
 
-    grunt.registerTask("default", ["shell:xmllint", "xsltproc", "shell:pdf", "shell:pdfanonyme", "jekyll:build", "htmlmin"]);
-    grunt.registerTask("dev", ["concurrent:dev"]);
-    grunt.registerTask("travis", ["jekyll:build", "htmlmin", "validation"]);
+    grunt.registerTask("dev", ["jekyll:serve"]);
+    grunt.registerTask("ci", ["jekyll:build", "htmlmin", "validation"]);
 };
